c2538f39272593a645e09d873107b9b81d34a91f
Gabriele Princiotta <gabriele.princiotta@igesa.it>
2021-02-10 15:31:44 +0100

    Adesso spamma sticker su whatsapp

--- app/scripts/theSpammer.js
+++ app/scripts/theSpammer.js
@@ -6,6 +6,7 @@
  */
 let messaggi; // Messaggi da inviare
 let stickerDaInviare; // Sticker da inviare
+let stickerAttuali; // Sticker attuali
 let modalita = 0; // Modalita' invio messaggi
 let chatOrig; // Chat quando il bot e' partito
 let msgIndex = 0; // Contatore del messaggio che sto attualmente mandando
@@ -17,7 +18,7 @@ let lastTime = 0; // Ultimo tempo impostato
 let spammerOnline = false; // Se il bot e' attivo
 let partito = false; // Se il bot e' partito almeno una volta
 let inPausa = false; // Se il bot e' stato messo in pausa
-let tipoInvio = 0; // 0 - messaggi     1 - stickers     2 - immagini
+let tipoInvio; // 0 - messaggi     1 - stickers     2 - immagini
 
 /**
  * Funzione per inviare una notifica toast con sweetalert
@@ -119,40 +120,59 @@ const checkChatName = (confronto = null) => {
 };
 
 const visualizzaStickers = async(time = 3000) => {
-    let button = document.getElementsByClassName('_37evB _16P6V _3x5p4 _3guyl')[0];
-    button.click();
-
-    await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(true);
-        }, time);
-    });
-
-
-    let stickers = [];
-
     try {
-        let images = document.getElementsByClassName('_1O6cA')[0].getElementsByClassName('_3Xjbn')[2].querySelectorAll('[tabindex="-1"]')[2].getElementsByTagName('img');
-        console.log('Immagini:');
-        console.log(images.length);
-
-        for (let i = 0; i < images.length; i++) {
-            stickers.push({
-                el: images[i],
-                url: images[i].src
-            });
+        let el = document.getElementsByClassName('_1O6cA')[0].getElementsByClassName('_3Xjbn')[2].querySelectorAll('[tabindex="-1"]')[2].getElementsByTagName('img');
+        if (el == null || typeof el == undefined) {
+            throw Exception('Sticker non aperti!');
         }
+
+        return stickerAttuali;
     } catch (e) {
         console.error(e);
-    }
+        let button = document.getElementsByClassName('_37evB _16P6V _3x5p4 _3guyl')[0];
+        button.click();
 
-    return stickers;
+        await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(true);
+            }, time);
+        });
+
+
+        let stickers = [];
+
+        try {
+            let images = document.getElementsByClassName('_1O6cA')[0].getElementsByClassName('_3Xjbn')[2].querySelectorAll('[tabindex="-1"]')[2].getElementsByTagName('img');
+
+            for (let i = 0; i < images.length; i++) {
+                stickers.push({
+                    el: images[i],
+                    url: images[i].src
+                });
+            }
+        } catch (e) {
+            console.error(e);
+        }
+
+        stickerAttuali = stickers;
+
+        return stickers;
+    }
 };
 
 const sendStickerBot = async(index) => {
-    spammerLog(`Invio sticker: ${stickerDaInviare[index].url}`);
+    spammerLog(`Invio sticker: ${stickerDaInviare[index]}`);
     await visualizzaStickers();
-    stickerDaInviare[index].el.click();
+    try {
+        all = stickerAttuali.filter((el) => {
+            return el.url == stickerDaInviare[index]
+        });
+        console.log(all);
+        all[0].el.click();
+    } catch (e) {
+        console.error(e);
+        spammerLog('Impossibile inviare lo sticker!');
+    }
 };
 
 /**
@@ -226,37 +246,47 @@ const sendMsgBot = (msg) => {
 const ohMyBot = () => {
     let chatAttuale = getChatName(); // Nome della chat attuale
     let msg; // Messaggio da inviare
+    let len;
+
     if (chatAttuale !== '') spammerLog(`Chat attuale: ${chatAttuale}`);
 
     if (checkChatName(chatOrig)) { // Se non ha cambiato chat ed e' aperta una chat oppure non siamo ne' su telegram web ne' su whatsapp web
+        switch (tipoInvio) {
+            case '0':
+                len = messaggi.length;
+                break;
+            case '1':
+                len = stickerDaInviare.length;
+                break;
+            case '2':
+                console.log(`Nessuna azione per il tipo invio: ${tipoInvio}`);
+                break;
+        }
+
+        spammerLog('len:');
+        spammerLog(len);
+
         if (modalita == 1) { // Se devo mandarlo in modo casuale
-            msgIndex = Math.round(Math.random() * (messaggi.length - 1)); // Genero un indice casuale come contatore
+            msgIndex = Math.round(Math.random() * (len - 1)); // Genero un indice casuale come contatore
         }
 
         switch (tipoInvio) {
-            case 0:
+            case '0':
                 msg = messaggi[msgIndex];
                 sendMsgBot(msg);
                 break;
-            case 1:
+            case '1':
                 sendStickerBot(msgIndex);
                 break;
-            case 2:
-
-                break;
+            default:
+                console.log(`Nessuna azione per il tipo invio: ${tipoInvio}`);
         }
 
         if (modalita == 0) { // Se a giro devo inviarli tutti
-            switch (tipoInvio) {
-                case 0:
-                    msgIndex = (msgIndex + 1) % (messaggi.length - 1);
-                    break;
-                case 1:
-                    msgIndex = (msgIndex + 1) % (stickerDaInviare.length - 1);
-                    break;
-                case 2:
-
-                    break;
+            if (msgIndex == (len - 1)) {
+                msgIndex = 0;
+            } else {
+                msgIndex++;
             }
         }
 
@@ -370,53 +400,73 @@ const resumeBot = () => {
  * Funzione che apre una finestra di dialogo per fare partire il bot in modalita' grafica
  */
 const dialogBot = async() => {
-    let sceltaStickers = ``;
+    let sceltaStickers = `<ul>`;
 
     if (window.location.href.includes('https://web.whatsapp.com')) { // Se siamo su Whatsapp Web
         let stick = await visualizzaStickers();
         for (let i = 0; i < stick.length; i++) {
-            sceltaStickers += `<img src="${stick[i].url}" width=33 height=33>`;
+            sceltaStickers += `<li>
+    <input type="checkbox" id="cb${i}" />
+    <label for="cb${i}" id="img-label"><img src="${stick[i].url}" width=100 height=100 /></label>
+</li>`;
         }
     }
 
+    sceltaStickers += `</ul>`;
+
     let span = document.createElement("span");
-    span.innerHTML = "<div class=\"swal-form\"> \
-    <p style=\"color:#000;\">Tipo invio<br><br></p> \
-    <select id=\"tipoInvio\" class=\"nice-input swal-form-field\" style=\"color:#000;background-color:#fff;\" onchange=\"cambiaSceltaTipo(this.value);\"> \
-        <option value=0 selected style=\"color:#000;background-color:#fff;\">Messaggi testuali</option> \
-        <option value=1 style=\"color:#000;background-color:#fff;\">Stickers</option> \
-    </select> \
-    <div id=\"invio-msg\"> \
-        <p style=\"color:#000;\"><br>Messaggi da inviare, ogni riga un messaggio<br><br></p> \
-        <textarea id=\"spammerText\" class=\"nice-input swal-form-field\" style=\"width:100%;color:#000;\"></textarea> \
-        <label for=\"spammerText\"></label> \
-    </div> \
-    <div id=\"invio-sticker\"> \
-        <p style=\"color:#000;\"><br>Stickers da inviare<br><br></p> \
-        <div id=\"scelta-stickers\" style=\"max-height:500px; overflow-y: auto;\">" + sceltaStickers + "</div> \
-    </div> \
-    <p style=\"color:#000;\"><br>Millisecondi da aspettare tra un messaggio e l'altro<br><br></p> \
-    <input id=\"spammerTime\" class=\"nice-input swal-form-field\" type=\"number\" name=\"\" value=1000 style=\"width:100%;color:#000;\"> \
-    <label for=\"spammerTime\"></label> \
-    <p style=\"color:#000;\"><br>Limite messaggi<br><br></p> \
-    <input id=\"spammerLimite\" class=\"nice-input swal-form-field\" type=\"number\" name=\"\" value=0 style=\"width:100%;color:#000;\"> \
-    <label for=\"spammerLimite\"></label> \
-    <p style=\"color:#000;\"><br>Modalita' invio<br><br></p> \
-    <select id=\"spammerMod\" class=\"nice-input swal-form-field\" style=\"color:#000;background-color:#fff;\"> \
-        <option value=0 selected style=\"color:#000;background-color:#fff;\">Invio di tutti i messaggi a giro</option> \
-        <option value=1 style=\"color:#000;background-color:#fff;\">Scelta causale tra i messaggi</option> \
-    </select> \
-    <label for=\"spammerMod\"></label> \
-</div>";
+    span.innerHTML = `<div class="swal-form">
+    <p>Tipo invio<br><br></p>
+    <select id="tipoInvio">
+        <option value="0" selected>Messaggi testuali</option>
+        <option value="1">Stickers</option>
+    </select>
+    <p><br>Modalita' invio<br><br></p>
+    <select id="spammerMod">
+        <option value=0 selected>Invio di tutti i messaggi a giro</option>
+        <option value=1>Scelta causale tra i messaggi</option>
+    </select>
+    <label for="spammerMod"></label>
+    <div id="invio-msg">
+        <p><br>Messaggi da inviare, ogni riga un messaggio<br><br></p>
+        <div class="box-container">
+            <div class="box">
+                <textarea id="spammerText" stzle="width: 100%;"></textarea>
+                <span></span>
+            </div>
+        </div>
+        <label for="spammerText"></label>
+    </div>
+    <div id="invio-sticker">
+        <p><br>Stickers da inviare<br><br></p>
+        <div id="scelta-stickers" style="max-height:300px; overflow-y: auto;">${sceltaStickers}</div>
+    </div>
+    <p><br>Millisecondi da aspettare tra un messaggio e l'altro<br><br></p>
+    <div class="box-container">
+        <div class="box">
+            <input id="spammerTime" type="number" name="" value=1000>
+            <span></span>
+        </div>
+    </div>
+    <label for="spammerTime"></label>
+    <p><br>Limite messaggi<br><br></p>
+    <div class="box-container">
+        <div class="box">
+            <input id="spammerLimite" type="number" name="" value=1000>
+            <span></span>
+        </div>
+    </div>
+    <label for="spammerLimite"></label>
+</div>`;
     swal({
-        title: 'TheSpammer',
+        title: 'The Spammer',
         content: span,
         buttons: {
             cancel: 'Annulla',
             confirm: 'Spamma'
         },
-        closeOnClickOutside: false,
-        closeOnEsc: false,
+        closeOnClickOutside: true,
+        closeOnEsc: true,
         preConfirm: () => {
             return new Promise((resolve, reject) => {
                 resolve({
@@ -436,9 +486,30 @@ const dialogBot = async() => {
                 let spammerLimite = document.getElementById('spammerLimite').value;
                 let spammerMod = document.getElementById('spammerMod').value;
                 tipoInvio = document.getElementById('tipoInvio').value;
-                if (spammerText !== "" && spammerTime > 0) { // Se ha inserito tutti i dati necessari
 
-                    let continuare = true; // Se continuare la creazione del nuovo processo
+                let cond;
+                let all = document.querySelectorAll('[id^="cb"]');
+                stickerDaInviare = [];
+
+                for (var i = 0; i < all.length; i++) {
+                    if (all[i].checked) {
+                        stickerDaInviare.push(all[i].parentElement.getElementsByTagName('img')[0].src);
+                    }
+                }
+
+                switch (tipoInvio) {
+                    case '0':
+                        cond = spammerText !== "" && spammerTime > 0;
+                        break;
+                    case '1':
+                        cond = stickerDaInviare.length > 0;
+                        break;
+                    default:
+                        cond = true;
+                        break;
+                }
+
+                if (cond) { // Se ha inserito tutti i dati necessari
                     if (spammerOnline) { // Se il bot e' in esecuzione
                         stopBot(); // Fermo quello attivo in questo momento
                     }
@@ -455,10 +526,7 @@ const dialogBot = async() => {
                     } else { // Altrimenti
                         tmp[0] = spammerText;
                     }
-                    console.log(spammerMod);
-                    console.log(spammerTime);
-                    console.log(tmp);
-                    console.log(lim);
+
                     startBot(spammerMod, spammerTime, "", tmp, lim);
 
                 } else { // Altrimenti
@@ -499,8 +567,198 @@ const dialogBot = async() => {
         spammerLog("Operazione annullata!");
     });
 
+    try {
+        document.getElementById('thespammer-alert').remove();
+    } catch (e) {
+        console.error(e);
+    }
+
     let styleElem = document.head.appendChild(document.createElement('style'));
-    styleElem.innerHTML = '.swal-overlay:before { height: 0% !important; }';
+    styleElem.id = 'thespammer-alert'
+    styleElem.innerHTML = `@import url('https://fonts.googleapis.com/css?family=Poppins');
+.swal-overlay:before {
+    height: 0% !important;
+}
+.swal-modal, .swal-icon--success:before, .swal-icon--success:after, .swal-icon--success:before, .swal-icon--success__hide-corners,
+.swal-icon--error:before, .swal-icon--error:after, .swal-icon--error:before, .swal-icon--error__hide-corners,
+.swal-icon--warning:before, .swal-icon--warning:after, .swal-icon--warning:before, .swal-icon--warning__hide-corners {
+    background-color: #000000;
+}
+.swal-content p, .swal-title, .swal-text {
+    color: #fff;
+}
+.swal-content *, .swal-title {
+    font-family: "Poppins" !important;
+}
+
+.swal-content select {
+    width: 100%;
+    height: 50px;
+    font-size: 100%;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0;
+    background-color: #c0392b;
+    border: none;
+    border-bottom: 2px solid #962d22;
+    color: white;
+    padding: 10px;
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    padding: 10px;
+}
+
+/* For IE <= 11 */
+.swal-content select::-ms-expand {
+    display: none; 
+}
+
+.swal-content ul {
+    list-style-type: none;
+}
+
+.swal-content li {
+    display: inline-block;
+}
+
+.swal-content input[type="checkbox"][id^="cb"] {
+    display: none;
+}
+
+.swal-content label#img-label {
+    border: 1px solid #fff;
+    padding: 10px;
+    display: block;
+    position: relative;
+    margin: 10px;
+    cursor: pointer;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+
+.swal-content label#img-label::before {
+    background-color: white;
+    color: white;
+    content: " ";
+    display: block;
+    border-radius: 50%;
+    border: 1px solid grey;
+    position: absolute;
+    top: -5px;
+    left: -5px;
+    width: 25px;
+    height: 25px;
+    text-align: center;
+    line-height: 28px;
+    transition-duration: 0.4s;
+    transform: scale(0);
+}
+
+.swal-content label#img-label img {
+    height: 100px;
+    width: 100px;
+    transition-duration: 0.2s;
+    transform-origin: 50% 50%;
+}
+
+.swal-content :checked+label#img-label {
+    border-color: #ddd;
+}
+
+.swal-content :checked+label#img-label::before {
+    content: "✓";
+    background-color: #c0392b;
+    transform: scale(1);
+}
+
+.swal-content :checked+label#img-label img {
+    transform: scale(0.9);
+    box-shadow: 0 0 5px #333;
+    z-index: -1;
+}
+
+$focusColor:#EF9F00;
+.box-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+
+  .box {
+    margin-bottom: 30px;
+    display: inline-block;
+    position: relative;
+    input {
+      padding: 5px 10px;
+      height: 45px;
+      font-size: 1em;
+      line-height: 45px;
+      border: 2px solid #ccc;
+      width: 400px;
+      outline: none;
+      background: #fff; 
+      color: #a2a5a7;
+      &:focus {                 
+        + span {      
+          width: 100%;
+          opacity: 1;
+          transform:translateX(0);
+          &:before {
+            top: 0;
+            left: 0;
+            width: 100%;
+          }
+          &:after {
+            bottom: 0;
+            left: 0;
+            width: 100%;
+          }
+        }
+      }
+      + span {
+          display: inline-block;
+          position: absolute;
+          left: 0;
+          top: 0;        
+          border-left: 2px solid $focusColor;
+          border-right: 2px solid $focusColor;
+          width: calc(100% + 100px);
+          transform:translateX(-50px);
+          height: 100%;
+          pointer-events: none;
+          opacity: 0;
+          transition-property:opacity, width;
+          transition: .3s ease-out;
+          
+          &:before,&:after{
+            content: "";
+            display: inline-block;
+            position: absolute;   
+            left: 25px;
+             transition-property:top, bottom;
+            transition: .3s ease-out;
+            height: 2px;
+            width: calc(100% - 50px);
+            background: $focusColor;
+          }
+          &:before {           
+            top: -30px;                        
+          }
+          &:after {
+            bottom: -30px;
+          }
+        }
+    }
+  }
+}
+
+`;
 
     return 0;
 };
