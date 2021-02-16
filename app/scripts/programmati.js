5ad348cab88e208d52d81d2cdbfea7c1c3ede951
Gabriele Princiotta <gabriele.princiotta@igesa.it>
2021-02-16 12:11:34 +0100

    Finita pagina gestione messaggi programmati, va molto bene, anche se per ora funziona solo su whatsapp web, da fare in futuro anche per gli altri social

--- app/scripts/programmati.js
+++ app/scripts/programmati.js
@@ -14,27 +14,124 @@ const getStorageData = key =>
         )
     );
 
+document.getElementById('elimina-tutti').addEventListener('click', () => {
+    if (confirm('Sicuro di volerli eliminare tutti?')) {
+        bbrowser.storage.local.set({
+            msgProgrammati: []
+        });
+        document.getElementById('elenco-messaggi').innerHTML = `<tr class="row100 body">
+        <td class="cell100 column1" colspan=4><span class="text-center">Nessun messaggio programmato!</span></td>
+    </tr>`;
+    }
+});
+
+const elimina = async(el, index) => {
+    if (confirm('Sicuro di voler eliminare il messaggio?')) {
+        let msgProgrammati;
+        try {
+            msgProgrammati = (await getStorageData('msgProgrammati')).msgProgrammati;
+
+            if (msgProgrammati == undefined) throw Exception();
+
+            msgProgrammati.splice(index, 1);
+        } catch (e) {
+            msgProgrammati = [];
+        }
+
+
+        bbrowser.storage.local.set({
+            msgProgrammati: msgProgrammati
+        });
+
+        el.parentElement.parentElement.remove();
+    }
+};
+
+const modifica = async(index, key, value, el, origClass) => {
+    let msgProgrammati;
+    try {
+        msgProgrammati = (await getStorageData('msgProgrammati')).msgProgrammati;
+
+        if (msgProgrammati == undefined) throw Exception();
+
+        msgProgrammati[index][key] = value;
+    } catch (e) {
+        msgProgrammati = [];
+    }
+
+    try {
+        bbrowser.storage.local.set({
+            msgProgrammati: msgProgrammati
+        });
+
+        el.className += ' td-success';
+    } catch (e) {
+        console.error(e);
+
+        el.className += ' td-error';
+    }
+
+    setTimeout(() => {
+        el.className = origClass;
+    }, 500);
+};
+
 document.addEventListener('DOMContentLoaded', async() => {
     try {
         if ((await getStorageData('temaScuro')).temaScuro) {
+            document.getElementById('tabella').className = 'table100 ver3 m-b-110';
             document.getElementById('style').setAttribute('href', 'programmati/css/stylesDark.css');
+        } else {
+            document.getElementById('tabella').className = 'table100 ver1 m-b-110';
         }
 
         const msgProgrammati = (await getStorageData('msgProgrammati')).msgProgrammati;
 
-        document.getElementById('elenco-messaggi').innerHTML = ``;
-        for (let i = 0; i < msgProgrammati.length; i++) {
-            document.getElementById('elenco-messaggi').innerHTML += `<li class="cd-accordion__item">
-    <a class="cd-accordion__label cd-accordion__label--icon-img" href="javascript: return false;" style="text-decoration: none;">
-        <span>${msgProgrammati[i].nome} - ${msgProgrammati[i].orario} &nbsp;
-            <img src="popup/images/icons/trash.png" width=33 height=33 onclick="" style="cursor: pointer;">
-            <img src="popup/images/icons/pencil.png" width=33 height=33 onclick="" style="cursor: pointer;">
-        </span>
-    </a>
-    <a class="cd-accordion__label cd-accordion__label--icon-img" href="javascript: return false;" style="text-decoration: none;">
-        <span class="text-sm" style="display: block;">${msgProgrammati[i].msg}</span>
-    </a>
-</li>`;
+        if (msgProgrammati.length > 0) {
+            document.getElementById('elenco-messaggi').innerHTML = ``;
+            for (let i = 0; i < msgProgrammati.length; i++) {
+                const orarioOrig = msgProgrammati[i].orario;
+                const split = orarioOrig.split('T');
+                let orario = `${split[0]} alle ${split[1]}`;
+                document.getElementById('elenco-messaggi').innerHTML += `<tr class="row100 body">
+    <td class="cell100 column1" contenteditable="true" id="msg-${i}">${msgProgrammati[i].msg}</td>
+    <td class="cell100 column2" contenteditable="true" id="nome-${i}">${msgProgrammati[i].nome}</td>
+    <td class="cell100 column3" contenteditable="true" id="orario-${i}">${orario}</td>
+    <td class="cell100 column4">
+        <button class="btn btn-danger" id="elimina-${i}">
+            <i class="fa fa-trash"></i>
+        </button>
+    </td>
+</tr>`;
+                setTimeout(() => {
+                    document.getElementById(`msg-${i}`).addEventListener('keypress', async function(e) {
+                        if (e.key === 'Enter' && !e.shiftKey) {
+                            e.preventDefault();
+                            await modifica(i, 'msg', this.innerHTML, this, 'cell100 column1');
+                        }
+                    });
+                    document.getElementById(`nome-${i}`).addEventListener('keypress', async function(e) {
+                        if (e.key === 'Enter') {
+                            e.preventDefault();
+                            await modifica(i, 'nome', this.innerText, this, 'cell100 column2');
+                        }
+                    });
+                    document.getElementById(`orario-${i}`).addEventListener('keypress', async function(e) {
+                        if (e.key === 'Enter') {
+                            e.preventDefault();
+                            const split = this.innerText.split(' alle ');
+                            await modifica(i, 'orario', `${split[0]}T${split[1]}`, this, 'cell100 column3');
+                        }
+                    });
+                    document.getElementById(`elimina-${i}`).addEventListener('click', async function() {
+                        await elimina(this, parseInt(this.id.split('-')[1]));
+                    });
+                }, 500);
+            }
+        } else {
+            document.getElementById('elenco-messaggi').innerHTML = `<tr class="row100 body">
+        <td class="cell100 column1" colspan=4><span class="text-center">Nessun messaggio programmato!</span></td>
+    </tr>`;
         }
     } catch (e) {
         console.error(e);
